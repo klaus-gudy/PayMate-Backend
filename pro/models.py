f4ac5b62ca8d05ef9c88798f5e77fbb13204787c
klaus-gudy <84222795+klaus-gudy@users.noreply.github.com>
2024-04-02 23:19:06 +0300

    Initial setup of the database model

--- pro/models.py
+++ pro/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Owner(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Property(models.Model):
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    location = models.CharField(max_length=255)
+    rent_amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Tenant(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
