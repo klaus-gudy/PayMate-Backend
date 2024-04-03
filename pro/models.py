6a714351b8fe9e8b7cc0f9bc66306ad0a897a33e
klaus-gudy <84222795+klaus-gudy@users.noreply.github.com>
2024-04-03 23:36:09 +0300

    🚧Chore: Added more details to model i.e maintainance request

--- pro/models.py
+++ pro/models.py
@@ -11,3 +11,18 @@ class Property(models.Model):
 
 class Tenant(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    contact = models.CharField(max_length=13)
+    emergency_contact = models.CharField(max_length=13)
+
+class RentalContract(models.Model):
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+class MaintenanceRequest(models.Model):
+    tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE)
+    request_date = models.DateField()
+    description = models.TextField()
+    status = models.CharField(max_length=20, choices=[('Pending', 'Pending'), ('InProgress', 'In Progress'), ('Completed', 'Completed')])
