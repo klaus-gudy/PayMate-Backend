8f2574cab5a8497320671a67fab77882cbd0a2f9
klaus-gudy <klausongudy@gmail.com>
2024-04-04 23:58:13 +0300

    Registered models into admin panel

--- pro/admin.py
+++ pro/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Property)
+admin.site.register(Tenant)
+admin.site.register(RentalContract)
+admin.site.register(MaintenanceRequest)
