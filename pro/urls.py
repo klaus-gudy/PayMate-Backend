85d289c78e548375fc6031c44c6e8950e0f44c2a
klaus-gudy <klausongudy@gmail.com>
2024-04-06 13:09:58 +0300

    Chore: Added path to the created view

--- pro/urls.py
+++ pro/urls.py
@@ -1,5 +1,16 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-    
+    path('owners/', OwnerListCreate.as_view()),
+    path('owner/<int:pk>/', OwnerRetrieveUpdateDestroy.as_view()),
+
+    path('properties/', PropertyListCreate.as_view()),
+    path('property/<int:pk>/', PropertyRetrieveUpdateDestroy.as_view()),
+
+    path('tenants/', TenantListCreate.as_view()),
+    path('tenant/<int:pk>/', TenantRetrieveUpdateDestroy.as_view()),
+
+    path('rental-contracts/', RentalContractListCreate.as_view()),
+    path('rental-contract/<int:pk>/', RentalContractRetrieveUpdateDestroy.as_view()),
 ]
