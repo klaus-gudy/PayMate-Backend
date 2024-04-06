a05d71001343d8655fc13f75414b168a02f1f0f0
klaus-gudy <klausongudy@gmail.com>
2024-04-06 13:09:37 +0300

    Chore: Defined rest view for visualization of data

--- pro/views.py
+++ pro/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import *
+from .serializers import *
 
-# Create your views here.
+class OwnerListCreate(generics.ListCreateAPIView):
+    queryset = Owner.objects.all()
+    serializer_class = OwnerSerializer
+
+class OwnerRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Owner.objects.all()
+    serializer_class = OwnerSerializer
+
+class PropertyListCreate(generics.ListCreateAPIView):
+    queryset = Property.objects.all()
+    serializer_class = PropertySerializer
+
+class PropertyRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Property.objects.all()
+    serializer_class = PropertySerializer
+
+class TenantListCreate(generics.ListCreateAPIView):
+    queryset = Tenant.objects.all()
+    serializer_class = TenantSerializer
+
+class TenantRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Tenant.objects.all()
+    serializer_class = TenantSerializer
+
+class RentalContractListCreate(generics.ListCreateAPIView):
+    queryset = RentalContract.objects.all()
+    serializer_class = RentalContractSerializer
+
+class RentalContractRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = RentalContract.objects.all()
+    serializer_class = RentalContractSerializer
+
