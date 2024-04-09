d4f161272c48499e5ad10b1ce0396eb965a482eb
klaus-gudy <klausongudy@gmail.com>
2024-04-09 23:38:31 +0300

    🚧Chore: Added user registration functionality

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .serializers import UserSerializer
+from .models import User
+
+class RegisterAPIView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+        
