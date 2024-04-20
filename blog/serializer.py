e8a6de709171e86038b6648efa51a6cfc042036a
klaus-gudy <klausongudy@gmail.com>
2024-04-20 20:00:37 +0300

    ⚰️Remove: deleted unnecessary files and code related to the blog and notifications apps

--- blog/serializer.py
+++
@@ -1,7 +0,0 @@
-from django.core import serializers
-from .models import Blog
-
-class BlogSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Blog
-        fields = '__all__'
--- pymate/settings.py
+++ pymate/settings.py
@@ -48,10 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'channels',
-    'blog',
     'pro',
     'users',
-    'notifications',
 ]
 
 MIDDLEWARE = [
