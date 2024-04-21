375c79d9c5b77fbcd8e262ff72fc0cca857fa15e
klaus-gudy <klausongudy@gmail.com>
2024-04-21 22:53:08 +0300

    👔Chore: Add new business logic functions to properties, tenants, and tenants near the end date

--- pro/views.py
+++ pro/views.py
@@ -3,6 +3,8 @@ from .models import *
 from .serializers import *
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from datetime import date
+from datetime import timedelta
 
 class OwnerListCreate(generics.ListCreateAPIView):
     authentication_classes = [JWTAuthentication]
@@ -33,6 +35,21 @@ class TenantRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
 def get_total_tenants(request):
     total_tenants = Tenant.objects.all().count()
     return total_tenants
+
+def get_total_properties(request):
+    total_properties = Property.objects.all().count()
+    return total_properties
+
+def get_total_tenants_with_contracts(request):
+    today = date.today()
+    total_rental_contracts = RentalContract.objects.filter(end_date__gte=today).count()
+    return total_rental_contracts
+
+def get_tenants_with_near_end_date(request):
+    today = date.today()
+    end_date = today + timedelta(days=30)
+    tenants = RentalContract.objects.filter(end_date__gte=today, end_date__lte=end_date)
+    return tenants
 
 class RentalContractListCreate(generics.ListCreateAPIView):
     queryset = RentalContract.objects.all()
