475b3b619940dcdbdff5e77629d1a4ea21e3170e
klaus gudy <klausongudy@gmail.com>
2024-05-16 19:23:18 +0300

    🧪Test: Celery tasks for asynchronous processing

--- pymate/settings.py
+++ pymate/settings.py
@@ -182,3 +182,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
